03cc2e780743564002abb01370efdaba04471776
Jon Maldia <jon@maldia.net>
2016-11-10 20:44:54 -0500

    Change email on index and show to name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :name, presence: true
 
   #relationship between pins and user
   has_many :pins
